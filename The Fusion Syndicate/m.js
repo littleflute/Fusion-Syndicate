e6e89524fe7e6facb43a54f8a5cf2e0d6b1cf12f
littleflute <13699175164@sina.cn>
2017-05-27 15:53:36 -0500

    Add files via upload

---
+++ The Fusion Syndicate/m.js
@@ -0,0 +1,9 @@
+var f = [];				
+f[0]	= "v0.0.7";			
+f[1]	= "01 Random Acts of Science.mp3";		
+f[2]	= "02 Stone Cold Infusion.mp3";		
+f[3]	= "03 Molecular Breakdown.mp3";		
+f[4]	= "04 Particle Accelerations.mp3";		
+f[5]	= "05 At the Edge of the Middle.mp3";		
+f[6]	= "06 Atom Smashing.mp3";		
+f[7]	= "07 In the Spirit Of.mp3";		
